e5fea6e2dbab062485b6af91e7d0f387210448e8
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-05 15:30:27 +0100

    Fix formatting issues in Level and Subject models, app component, sidebar component, responsePopup component, playquiz component, modal component, quiz-page component, level-page component, and media-page component

--- src/app/Models/Level.ts
+++ src/app/Models/Level.ts
@@ -1,7 +1,7 @@
 import Question from "./Question";
 
 export default class Level {
-    id: number = 0;
+    id: number  = 0;
     description: string = "";
     maxPoints: number = 0;
     minPoints: number = 0;
--- src/app/Models/Subject.ts
+++ src/app/Models/Subject.ts
@@ -2,7 +2,7 @@ import Question from "./Question";
 import TopSubject from "./TopSubject";
 
 export default class Subject {
-    id: number = 0;
+    id: number = 0 ;
     name: string = "";
     top?: TopSubject;
     subs: Subject[] = [];
