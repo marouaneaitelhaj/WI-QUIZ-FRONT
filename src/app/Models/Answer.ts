552315f9bf0d1905577bab42789a8dd5017f28bd
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-05 12:24:53 +0100

    Fix radio button click event and add alert component

---
+++ src/app/Models/Answer.ts
@@ -0,0 +1,6 @@
+import Validation from "./Validation";
+
+export default class Answer {
+    id: number = 0;
+    validation: Validation = new Validation();
+}
--- src/app/Pages/playquiz/playquiz.component.ts
+++ src/app/Pages/playquiz/playquiz.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Answer from 'src/app/Models/Answer';
 import Question from 'src/app/Models/Question';
 import Quiz from 'src/app/Models/Quiz';
 import Response from 'src/app/Models/Response';
+import Validation from 'src/app/Models/Validation';
 import { MyResponse } from 'src/app/Response/Response';
 import { QuizService } from 'src/app/Services/quiz.service';
 
@@ -13,10 +15,15 @@ import { QuizService } from 'src/app/Services/quiz.service';
   providers: [QuizService]
 })
 export class PlayquizComponent {
+  showAlert: boolean = false;
+  needConfirm: boolean = false;
+  message: string = "rak nadi";
   id: number = 0;
+  questionNumber: number = 0;
   quiz: Quiz = new Quiz();
-  selectedImage : number = 0;
+  selectedImage: number = 0;
   question: Question = new Question();
+  answers: Answer[] = [];
   constructor(private route: ActivatedRoute, private quizService: QuizService) { }
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -24,8 +31,28 @@ export class PlayquizComponent {
     });
     this.quizService.findById(this.id).subscribe((data: Quiz) => {
       this.quiz = data;
-      this.question = this.quiz.questionOfQuizs[0].question;
+      this.question = this.quiz.questionOfQuizs[this.questionNumber].question;
       console.log(this.question);
     });
+  }
+  nextquestion() {
+    if (this.questionNumber == this.quiz.questionOfQuizs.length - 1) {
+      this.showAlert = true;
+      this.message = "آخرین سوال است";
+      var result = 0;
+      this.answers.forEach((answer: Answer) => {
+        if (answer.validation.correct == true) {
+          result = result + answer.validation.points;
+        }
+      });
+      console.log(result);
+    } else {
+      this.questionNumber++;
+      this.question = this.quiz.questionOfQuizs[this.questionNumber].question;
+    }
+  }
+  setResponse(response : Validation){
+    this.answers[this.questionNumber] = new Answer();
+    this.answers[this.questionNumber].validation = response;
   }
 }
