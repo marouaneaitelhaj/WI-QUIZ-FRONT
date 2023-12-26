c04bf2c259a5f7b2e5015fd29eaa8b3ba21cefff
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-26 12:16:37 +0100

    Add Primeng styles and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
@@ -70,6 +70,7 @@ import { AssignQuizPopupComponent } from './Components/assignQuizPopup/assignQui
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
     // StoreModule.forRoot({message : myReducer})
   ],
   providers: [],
