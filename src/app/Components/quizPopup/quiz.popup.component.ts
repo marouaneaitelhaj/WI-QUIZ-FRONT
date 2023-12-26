63dfd8f800c22ddcc5a083e527d39a5f3d1e8713
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-26 16:17:24 +0100

    Add QuizService dependency to QuizPopupComponent constructor

--- src/app/Components/quizPopup/quiz.popup.component.ts
+++ src/app/Components/quizPopup/quiz.popup.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { FunctionType } from 'src/app/Enums/FunctionType';
 import Quiz from 'src/app/Models/Quiz';
 import Teacher from 'src/app/Models/Teacher';
+import { QuizService } from 'src/app/Services/quiz.service';
 import { TeacherService } from 'src/app/Services/teacher.service';
 @Component({
   selector: 'app-quiz-popup',
@@ -15,7 +16,7 @@ export class QuizPopupComponent implements OnChanges {
   @Input() quiz: Quiz = new Quiz();
   teachers: Observable<Teacher[]> = this.teacherService.teachers;
   quizPopupForm : FormGroup = new FormBuilder().group({});
-  constructor(private teacherService : TeacherService){}
+  constructor(private teacherService : TeacherService, private quizService : QuizService){}
   ngOnChanges(changes: SimpleChanges): void {
     this.quizPopupForm = new FormBuilder().group({
       successResult : [this.quiz?.successResult || '',Validators.required],
@@ -24,12 +25,18 @@ export class QuizPopupComponent implements OnChanges {
       numberOfChances : [this.quiz?.numberOfChances || '',Validators.required],
       comment : [this.quiz?.comment || '',Validators.required],
       teacher_id : [this.quiz?.teacher.id || '',Validators.required],
+      id : [this.quiz?.id || 0],
     });
   }
   togglePopUp() {
     this.show.emit(false);
   }
   submit() {
+    if(this.quizPopupForm.value.id == 0){
+      this.quizService.save(this.quizPopupForm.value);
+    }else{
+      this.quizService.update(this.quizPopupForm.value);
+    }
     this.togglePopUp();
   }
 }
