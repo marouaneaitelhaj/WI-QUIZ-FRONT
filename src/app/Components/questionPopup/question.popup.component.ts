af7b0c71b20f6984a5c626086fb24ee516d5aef7
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-26 10:08:46 +0100

    Add OnChanges interface and form validation to QuestionPopupComponent

--- src/app/Components/questionPopup/question.popup.component.ts
+++ src/app/Components/questionPopup/question.popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { FunctionType } from 'src/app/Enums/FunctionType';
 import { QuestionType } from 'src/app/Enums/QuestionType';
 import Level from 'src/app/Models/Level';
@@ -7,34 +7,54 @@ import Question from 'src/app/Models/Question';
 import { MyResponse } from 'src/app/Response/Response';
 import { LevelService } from 'src/app/Services/level.service';
 import { SubjectService } from 'src/app/Services/subject.service';
+import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { QuestionService } from 'src/app/Services/question.service';
 @Component({
   selector: 'app-question-popup',
   templateUrl: './question.popup.component.html',
   styleUrls: ['./question.popup.component.css']
 })
-export class QuestionPopupComponent {
+export class QuestionPopupComponent implements OnChanges {
   @Output() show = new EventEmitter<boolean>();
   @Input() question: Question = {} as Question;
-  levels: Level[] = [];
-  subjects: Subject[] = [];
+  levels: Observable<Level[]> = this.levelService.levels;
+  subjects: Observable<Subject[]> = this.subjectService.subjects;
   keysOfQuestionTypes = Object.keys(QuestionType);
   togglePopUp() {
     this.show.emit(false);
   }
   submit() {
+    if (this.questionPopupForm.value.id === 0) {
+      this.questionService.save(this.questionPopupForm.value)
+    } else {
+      this.questionService.update(this.questionPopupForm.value)
+    }
     this.togglePopUp();
   }
-  constructor(private levelService: LevelService,private subjectService:SubjectService) {}
-  ngAfterContentInit() {
-    this.levelService.levels.subscribe(
-      (levels) => {
-        this.levels = levels;
-      }
-    );
-    this.subjectService.subjects.subscribe(
-      (subjects) => {
-        this.subjects = subjects;
-      }
-    );
+  questionPopupForm: FormGroup = new FormBuilder().group({
+    id: [this.question.id || 0],
+    question: [this.question.question || '', Validators.required],
+    numberOfAnswers: [this.question.numberOfAnswers || 0, Validators.required],
+    numberOfCorrectAnswers: [this.question.numberOfCorrectAnswers || 0, Validators.required],
+    questionType: [this.question.questionType || QuestionType.SINGLE_CHOICE, Validators.required],
+    points: [this.question.points || 0, Validators.required],
+    time: [this.question.time || 0, Validators.required],
+    level_id: [this.question?.level?.id || 0, Validators.required],
+    subject_id: [this.question?.subject?.id || 0, Validators.required],
+  });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.questionPopupForm = new FormBuilder().group({
+      id: [this.question.id || 0],
+      question: [this.question.question || '', Validators.required],
+      numberOfAnswers: [this.question.numberOfAnswers || 0, Validators.required],
+      numberOfCorrectAnswers: [this.question.numberOfCorrectAnswers || 0, Validators.required],
+      questionType: [this.question.questionType || QuestionType.SINGLE_CHOICE, Validators.required],
+      points: [this.question.points || 0, Validators.required],
+      time: [this.question.time || 0, Validators.required],
+      level_id: [this.question?.level?.id || 0, Validators.required],
+      subject_id: [this.question?.subject?.id || 0, Validators.required],
+    });
   }
+  constructor(private questionService: QuestionService, private levelService: LevelService, private subjectService: SubjectService) { }
 }
