82e3827dde623b9b66110d36363945583206b497
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-26 15:59:11 +0100

    Refactor media popup component and add form validation

--- src/app/Components/mediaPopup/media.popup.component.ts
+++ src/app/Components/mediaPopup/media.popup.component.ts
@@ -1,35 +1,54 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { FunctionType } from 'src/app/Enums/FunctionType';
 import { MediaType } from 'src/app/Enums/MediaType';
 import Media from 'src/app/Models/Media';
 import Question from 'src/app/Models/Question';
+import { MediaService } from 'src/app/Services/media.service';
 import { QuestionService } from 'src/app/Services/question.service';
 @Component({
   selector: 'app-media-popup',
   templateUrl: './media.popup.component.html',
   styleUrls: ['./media.popup.component.css']
 })
-export class MediaPopupComponent {
+export class MediaPopupComponent implements OnChanges {
   @Output() show = new EventEmitter<boolean>();
-  questions: Question[] = [];
-  constructor(private questionService : QuestionService){}
+  questions: Observable<Question[]> = this.questionService.questions;
   mediasType = MediaType;
   mediasTypeKeys = Object.keys(this.mediasType);
   @Input() media: Media = new Media();
-  setfile(event : any){
+  setfile(event: any) {
     this.media.src = event.target.files[0];
   }
   togglePopUp() {
     this.show.emit(false);
   }
   submit() {
+    this.mediaService.upload(this.media.src).subscribe((data: any) => {
+      this.media.src = data.url;
+      this.mediaPopUpForm.value.src = data.url;
+      if (this.mediaPopUpForm.value.id == 0) {
+        this.mediaService.save(this.mediaPopUpForm.value)
+      } else {
+        this.mediaService.update(this.mediaPopUpForm.value)
+      }
+    }, (error) => { });
     this.togglePopUp();
   }
-  ngAfterContentInit() {
-    this.questionService.questions.subscribe(
-      (questions) => {
-        this.questions = questions;
-      }
-    );
+  constructor(private questionService: QuestionService, private mediaService: MediaService) { }
+  mediaPopUpForm: FormGroup = new FormBuilder().group({
+    question_id: [this.media.question.id || '', Validators.required],
+    type: [this.media.type || '', Validators.required],
+    id: [this.media.id || 0],
+    src: [this.media.src || ''],
+  });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.mediaPopUpForm = new FormBuilder().group({
+      question_id: [this.media?.question?.id || '', Validators.required],
+      type: [this.media?.type || '', Validators.required],
+      id: [this.media?.id || 0],
+      src: [this.media.src || ''],
+    });
   }
 }
