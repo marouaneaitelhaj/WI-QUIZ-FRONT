ae5c70f5eb9b183cd3bda66e01926b6ee25d6844
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-12-26 10:29:51 +0100

    Refactor response and validation popups

--- src/app/Components/responsePopup/response.popup.component.ts
+++ src/app/Components/responsePopup/response.popup.component.ts
@@ -1,18 +1,37 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FunctionType } from 'src/app/Enums/FunctionType';
 import Response from 'src/app/Models/Response';
+import { QuestionService } from 'src/app/Services/question.service';
+import { ResponseService } from 'src/app/Services/response.service';
 @Component({
   selector: 'app-response-popup',
   templateUrl: './response.popup.component.html',
   styleUrls: ['./response.popup.component.css']
 })
-export class ResponsePopupComponent {
+export class ResponsePopupComponent implements OnChanges {
   @Output() show = new EventEmitter<boolean>();
   @Input() response: Response = new Response();
+  responsePopupForm: FormGroup = new FormBuilder().group({
+    id: [this.response.id || 0],
+    response: [this.response.response || '', Validators.required],
+  });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.responsePopupForm = new FormBuilder().group({
+      id: [this.response.id || 0],
+      response: [this.response.response || '', Validators.required],
+    });
+  }
   togglePopUp() {
     this.show.emit(false);
   }
   submit() {
+    if (this.responsePopupForm.value.id === 0) {
+      this.responseService.save(this.responsePopupForm.value)
+    } else {
+      this.responseService.update(this.responsePopupForm.value)
+    }
     this.togglePopUp();
   }
+  constructor(private responseService: ResponseService) { }
 }
--- src/app/Components/validationPopup/validation.popup.component.ts
+++ src/app/Components/validationPopup/validation.popup.component.ts
@@ -1,38 +1,49 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { FunctionType } from 'src/app/Enums/FunctionType';
 import Question from 'src/app/Models/Question';
 import Response from 'src/app/Models/Response';
 import Validation from 'src/app/Models/Validation';
 import { QuestionService } from 'src/app/Services/question.service';
 import { ResponseService } from 'src/app/Services/response.service';
+import { ValidationService } from 'src/app/Services/validation.service';
 @Component({
   selector: 'app-validation-popup',
   templateUrl: './validation.popup.component.html',
   styleUrls: ['./validation.popup.component.css']
 })
-export class ValidationPopupComponent {
+export class ValidationPopupComponent implements OnChanges {
   @Output() show = new EventEmitter<boolean>();
-  @Input() validations: Validation[] = [];
   @Input() validation: Validation = new Validation();
-  questions: Question[] = [];
-  responses: Response[] = [];
-  constructor(private questionService : QuestionService, private responseService : ResponseService){}
+  questions: Observable<Question[]> = this.questionService.questions;
+  responses: Observable<Response[]> = this.responseService.responses;
+  validationPopupForm: FormGroup = new FormBuilder().group({
+    id: [this.validation.id || 0],
+    question_id: [this.validation.question.id || 0, Validators.required],
+    response_id: [this.validation.response.id || 0, Validators.required],
+    isCorrect: [this.validation.correct || false, Validators.required],
+    points: [this.validation.points || 0, Validators.required],
+  });
+  ngOnChanges(changes: SimpleChanges): void {
+    this.validationPopupForm = new FormBuilder().group({
+      id: [this.validation.id || 0],
+      question_id: [this.validation.question.id || 0, Validators.required],
+      response_id: [this.validation.response.id || 0, Validators.required],
+      isCorrect: [this.validation.correct || false, Validators.required],
+      points: [this.validation.points || 0, Validators.required],
+    });
+  }
+  constructor(private questionService: QuestionService, private validationService: ValidationService, private responseService: ResponseService) { }
   togglePopUp() {
     this.show.emit(false);
   }
   submit() {
+    if (this.validationPopupForm.value.id === 0) {
+      this.validationService.save(this.validationPopupForm.value)
+    } else {
+      this.validationService.update(this.validationPopupForm.value)
+    }
     this.togglePopUp();
-  }
-  ngAfterContentInit() {
-    this.questionService.questions.subscribe(
-      (questions) => {
-        this.questions = questions;
-      }
-    );
-    this.responseService.responses.subscribe(
-      (responses) => {
-        this.responses = responses;
-      }
-    );
   }
 }
